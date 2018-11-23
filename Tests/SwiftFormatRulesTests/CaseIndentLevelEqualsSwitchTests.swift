5f1b8543eed7124c9a5b1e8b1748f2e131981480
Tony Allevato <tony.allevato@gmail.com>
2018-11-23 15:33:14 -0800

    Make case indentation rule lint-only.
    The pretty printer will handle reïndenting cases; this also fixes a bug
    where comments between cases were being discarded.

--- Tests/SwiftFormatRulesTests/CaseIndentLevelEqualsSwitchTests.swift
+++ Tests/SwiftFormatRulesTests/CaseIndentLevelEqualsSwitchTests.swift
@@ -1,71 +1,52 @@
-import Foundation
 import XCTest
-import SwiftSyntax
 
 @testable import SwiftFormatRules
 
 public class CaseIndentLevelEqualsSwitchTests: DiagnosingTestCase {
   public func testsInvalidCaseIndent() {
-    XCTAssertFormatting(
-      CaseIndentLevelEqualsSwitch.self,
-      input: """
-             switch order {
-              
-             case .ascending:
-               print("Ascending")
-                          case .descending:
-               print("Descending")
-                case .same:
-               print("Same")
-             }
-             """,
-      expected: """
-                switch order {
-                
-                case .ascending:
-                  print("Ascending")
-                case .descending:
-                  print("Descending")
-                case .same:
-                  print("Same")
-                }
-                """)
+    let input =
+      """
+      switch order {
+
+      case .ascending:
+        print("Ascending")
+                   case .descending:
+        print("Descending")
+         case .same:
+        print("Same")
+      }
+      """
+
+    performLint(CaseIndentLevelEqualsSwitch.self, input: input)
+    XCTAssertDiagnosed(.adjustCaseIndentation(by: -13))
+    XCTAssertDiagnosed(.adjustCaseIndentation(by: -3))
   }
   
   public func testsInvalidNestedCaseIndent() {
-    XCTAssertFormatting(
-      CaseIndentLevelEqualsSwitch.self,
-      input: """
-             if true {
-               switch order {
-             case .ascending:
-                 print("Ascending")
-                          case .descending:
-                 print("Descending")
-                case .same:
-                 print("Same")
-               }
-             }
-             """,
-      expected: """
-                if true {
-                  switch order {
-                  case .ascending:
-                    print("Ascending")
-                  case .descending:
-                    print("Descending")
-                  case .same:
-                    print("Same")
-                  }
-                }
-                """)
+    let input =
+      """
+      if true {
+        switch order {
+      case .ascending:
+          print("Ascending")
+                   case .descending:
+          print("Descending")
+         case .same:
+          print("Same")
+        }
+      }
+      """
+
+    performLint(CaseIndentLevelEqualsSwitch.self, input: input)
+    XCTAssertDiagnosed(.adjustCaseIndentation(by: 2))
+    XCTAssertDiagnosed(.adjustCaseIndentation(by: -11))
+    XCTAssertDiagnosed(.adjustCaseIndentation(by: -1))
   }
   
   #if !os(macOS)
   static let allTests = [
     CaseIndentLevelEqualsSwitchTests.testsInvalidCaseIndent,
     CaseIndentLevelEqualsSwitchTests.testsInvalidNestedCaseIndent
-    ]
+  ]
   #endif
-  
 }
